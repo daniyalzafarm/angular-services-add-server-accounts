f09f008fb038bded80a30e7f39b2050692b15cae
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2022-01-10 21:34:26 +0500

    Add accounts Service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,23 +1,29 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountsService } from '../services/accounts.service';
 import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
+  // providers: [LoggingService, AccountsService], // AccountsService is inherited from parent
   providers: [LoggingService],
 })
 export class AccountComponent {
   @Input() account!: { name: string; status: string };
   @Input() id!: number;
-  @Output() statusChanged = new EventEmitter<{
-    id: number;
-    newStatus: string;
-  }>();
+  // @Output() statusChanged = new EventEmitter<{
+  //   id: number;
+  //   newStatus: string;
+  // }>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {}
   onSetTo(status: string) {
-    this.statusChanged.emit({ id: this.id, newStatus: status });
+    this.accountsService.accounts[this.id].status = status;
+    // this.statusChanged.emit({ id: this.id, newStatus: status });
     this.loggingService.logStatusChange(status);
     // console.log('A server status changed, new status: ' + status);
   }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,21 +1,30 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from '../services/accounts.service';
 import { LoggingService } from '../services/logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService, AccountsService], // AccountsService is inherited from parent
   providers: [LoggingService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
+  // @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
   // It informs angular that we will need an instance of this loggingSevice
-  constructor(private loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {}
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
+    this.accountsService.accounts.push({
       name: accountName,
       status: accountStatus,
     });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus,
+    // });
     this.loggingService.logStatusChange(accountStatus);
     // console.log('A server status changed, new status: ' + accountStatus);
   }
